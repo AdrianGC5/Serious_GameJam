420e4b8ae664984ea8646f215ec15297eda5e2e0
AdrianGC5 <arnaud.adrian.almeida@gmail.com>
2026-06-24 09:31:28 -0300

    Revert "adfasdas"
    This reverts commit 0f6eff8f4f08c8ddd322ca118ba75b206d21780c.

---
+++ spritesa.gd
@@ -0,0 +1,19 @@
+extends Sprite2D
+
+const RumSpt = preload("res://bloc.png")
+const MilkSpt = preload("res://temp assets/spin_wheel_button.png")
+
+func _ready() -> void:
+	pass
+		
+ 
+
+
+
+
+func _on_drinkger_rum() -> void:
+	self.texture = RumSpt
+
+
+func _on_drinkger_milk() -> void:
+	self.texture = MilkSpt
